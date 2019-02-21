0142da09cb257a2df8e4ee62bef44a024d38f36d
rdeffayet <romain.deffayet@gmail.com>
2019-02-21 12:23:31 +0100

    Finished setting up the network, needs testing

--- lstm.py
+++ lstm.py
@@ -1,22 +1,23 @@
 from __future__ import print_function
 import numpy as np
 
-from keras.optimizers import SGD
 from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras.layers.recurrent import LSTM
 
-# Seed for reproductibility
+### Seed for reproductibility
 np.random.seed(1234)
 
+
+### Hyperparameters
 batch_size = 5
 hidden_units = 32
 nb_classes = 2
+
+### Data
 print('Loading data...')
-##########################################"
-(X_train, y_train), (X_test, y_test) = GET_THE_DATA_PIETRO_WHAT_THE_FUCK [nb_sequences * sequence length * nb_variables]
-##########################################
+(X_train, y_train), (X_test, y_test) = GET_THE_DATA
 
 print(len(X_train), 'train sequences')
 print(len(X_test), 'test sequences')
@@ -30,15 +31,22 @@ print('Build model...')
 Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
 
+
+### LSTM
 model = Sequential()
 model.add(LSTM(hidden_units, batch_input_size = (50, X_train.shape[1], X_train.shape[2]) ))
 
+
+### Classification
 model.add(Dense(nb_classes))
 model.add(Activation('softmax'))
 
-sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
+### Learning algorithm
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+
+### Training and evualuation
 print("Training ...")
 model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=3, validation_data=(X_test, y_test), show_accuracy=True)
 score, acc = model.evaluate(X_test, y_test,
