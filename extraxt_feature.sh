9183cb481c0c8b6efced5226aa361f721a92ceac
Gabriel de Lisle <gldl@kth.se>
2019-02-21 15:01:02 +0100

    merging convert and extract scripts

--- extraxt_feature.sh
+++ extraxt_feature.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+
 for element in swedish skane norwegian danish
 do
   echo "Extractiong for: $element ..."
   cd $element;
+  ./../scripts/convert.sh;
+  rm -f *.mp3;
   ./../scripts/extract_filenames.sh;
   ls --width=1 *.wav > list.txt;
   ./../scripts/extract_pitch.sh;
