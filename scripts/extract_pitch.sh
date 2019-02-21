e477039340e0792cf66aa20918fff77147fa716f
Pietro Alovisi <pietro.alovisi@mail.polimi.it>
2019-02-21 16:08:39 +0100

    other results

--- scripts/extract_pitch.sh
+++ scripts/extract_pitch.sh
@@ -6,6 +6,7 @@ package require snack
 
 snack::sound s
 
+set frame_l 0.05
 set f [open list.txt]
 set list [read $f]
 close $f
@@ -14,15 +15,15 @@ foreach file $list {
  s read $file
 
  set fd [open [file rootname $file].f0 w]
- puts $fd [join [s pitch -method esps] \n]
+ puts $fd [join [s pitch -method esps -framelength $frame_l] \n]
  close $fd
 
  set fd [open [file rootname $file].frm w]
- puts $fd [join [s formant] \n]
+ puts $fd [join [s formant -framelength $frame_l] \n]
  close $fd
 
  set fd [open [file rootname $file].pwr w]
- puts $fd [join [s power] \n]
+ puts $fd [join [s power -framelength $frame_l] \n]
  close $fd
 }
 
