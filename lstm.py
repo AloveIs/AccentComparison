39674ebf0b20b5fa26600a71eac18fe2e80268a3
Romain Deffayet <romain.deffayet@gmail.com>
2019-02-23 15:56:31 +0100

    Simple LSTM works on EGG_200
    Need to use our own data

--- lstm.py
+++ lstm.py
@@ -5,14 +5,16 @@ from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras.layers.recurrent import LSTM
+from keras.callbacks import EarlyStopping
+from keras.optimizers import Adam
 
 ### Seed for reproductibility
-np.random.seed(1234)
+np.random.seed(123)
 
 
 ### Hyperparameters
 batch_size = 10
-hidden_units = 32
+hidden_units = 16
 nb_classes = 2
 
 ### Data
@@ -49,17 +51,25 @@ def get_data(label):
 
 
 def train_model(X_train, X_val, X_test, y_train,y_val, y_test):
+    print('Preprocessing...')
+    
+    print("Mean of training set : ", np.mean(X_train))    
+    print("Mean of validation set : ", np.mean(X_val)) 
+    print("Mean of test set : ", np.mean(X_test)) 
+    
     print('Build model...')
 
-    Y_train = np_utils.to_categorical(y_train, nb_classes)
-    Y_val = np_utils.to_categorical(y_val, nb_classes)
-    Y_test = np_utils.to_categorical(y_test, nb_classes)
+    Y_train = np_utils.to_categorical(np.clip(y_train, 0, 1), nb_classes)
+    Y_val = np_utils.to_categorical(np.clip(y_val, 0, 1), nb_classes)
+    Y_test = np_utils.to_categorical(np.clip(y_test, 0, 1), nb_classes)
     
 
 
     ### LSTM
     model = Sequential()
-    model.add(LSTM(hidden_units, batch_input_shape = (batch_size, X_train.shape[1], X_train.shape[2]) ))
+    model.add(LSTM(hidden_units, 
+                   batch_input_shape = (batch_size, X_train.shape[1], X_train.shape[2]), 
+                   kernel_initializer = 'random_uniform' ))
 
 
     ### Classification
@@ -68,18 +78,24 @@ def train_model(X_train, X_val, X_test, y_train,y_val, y_test):
 
 
     ### Learning algorithm
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer=Adam(lr = 0.001), metrics=['accuracy'])
 
 
     ### Training and evualuation
     print("Training ...")
-    model.fit(X_train, Y_train, batch_size=batch_size, epochs=5, validation_data=(X_val, Y_val), verbose = 1)
-    '''[score, acc] = model.evaluate(X_test, Y_test,
+    model.fit(X_train, Y_train,
+              batch_size=batch_size, epochs=10,
+              validation_data=(X_val, Y_val), 
+              #callbacks = [EarlyStopping(patience = 2, verbose = 1)],
+              verbose = 1)
+    [score, acc] = model.evaluate(X_test, Y_test,
                                 batch_size=batch_size,
-                                verbose = 1)'''
-    prediction = model.predict(X_test, batch_size = batch_size)
-    #print('Test score:', score)
-    print('Test accuracy:', 1 - len(np.nonzero(np.argmax(prediction, axis = 1) - y_test)[0]) / 50)
+                                verbose = 0)
+    #prediction = model.predict(X_test, batch_size = batch_size)
+    #print(prediction)
+    print('Test score : %.3f' % score)
+    print('Test accuracy : %.2f %%' % (acc*100))
+    #print('Test accuracy: %.2f %%' % (100 - len(y_test[np.nonzero(np.argmax(prediction, axis = 1) - y_test)]) *100 / 50))
     
 if __name__ == "__main__":
     X_train, X_val, X_test, y_train, y_val, y_test = get_data("test")
