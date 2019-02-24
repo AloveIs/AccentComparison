8c21a8460b2c5c68738c3d51ff51562e350b11a8
Romain Deffayet <romain.deffayet@gmail.com>
2019-02-24 13:06:33 +0100

    Ran the lstm on our data
    Fully operational pipeline from data to classification.
    Need for better data now

--- gather.py
+++ gather.py
@@ -3,20 +3,19 @@ import os
 
 
 def gather(folder, features) :
-	with open(os.path.join(folder,"name_list.txt")) as file :
-		data = np.array([[np.load(os.path.join(folder,line[:-1]+f+".npy")) for f in features] for line in file])
-	N_samples = min([d[0].shape[0] for d in data])
-	print("cut each sequence to", N_samples, "samples")
-	N_sequences, N_features = data.shape
-	new_format = np.zeros((N_sequences, N_samples, N_features))
-	for i in range(N_sequences) :
-		for j in range(N_samples) :
-			for k in range(N_features) :
-				new_format[i,j,k] = data[i,k][j]
-	return new_format
+    with open(os.path.join(folder,"name_list.txt")) as file :
+        data = np.array([[np.load(os.path.join(folder,line[:-1]+f+".npy")) for f in features] for line in file])
+    N_samples = min([d[0].shape[0] for d in data])
+    print("cut each sequence to", N_samples, "samples")
+    N_sequences, N_features = data.shape
+    new_format = np.zeros((N_sequences, 814, N_features))
+    for i in range(N_sequences) :
+        for j in range(814) :
+            for k in range(N_features) :
+                new_format[i,j,k] = data[i,k][j]
+    return new_format
 
 
 if __name__ == '__main__':
-	data = gather("norwegian", [ "pitch", "voice", "pwr" ])
-	print(data.shape)
-	
+    data = gather("norwegian", [ "pitch", "voice", "pwr" ])
+    print(data.shape)
--- lstm.py
+++ lstm.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import numpy as np
 
+from sklearn.model_selection import train_test_split
+
 from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
@@ -15,7 +17,7 @@ np.random.seed(123)
 
 
 ### Hyperparameters
-batch_size = 10
+batch_size = 1
 hidden_units = 16
 nb_classes = 2
 
@@ -37,47 +39,51 @@ def get_data(label):
         y_val = y_test[:50]
         y_test = y_test[50:]
 
-    elif label=="south":
+    elif label=="west_skane":
         #data1 = gather("norwegian", ["pitch", "voice", "pwr"])#N_sequences, N_samples, N_features
         #data2 = gather("west", ["pitch", "voice", "pwr"])
-        data1 = gather("norwegian", ["pitch"])
-        data2 = gather("west", ["pitch"])
-        data = np.concatenate((data1, data2))
-        y_label = np.concatenate((np.ones(data1.shape[0]), -1 * np.ones(data2.shape[0])))
-        print(data1.shape, data2.shape, data.shape)
-        ## shuffle and split the data to train validation and test
+        data1 = gather("skane", ["pitch"])
+        data2 = gather("west", ["pitch"])[:12, : , :]
+        X = np.concatenate((data1, data2))
+        y = np.concatenate(([1] * len(data1), [0] * len(data2)))
+        #y_label = np.concatenate((np.ones(data1.shape[0]), -1 * np.ones(data2.shape[0])))
+        print("Skåne Dataset : ", data1.shape)
+        print("Western Sweden Dataset : ", data2.shape)
+        ## Shuffle and split the data to train validation and test
+        X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle = True)
 
 
     elif label=="west":
         data1 = gather("skane", ["pitch", "voice", "pwr"])
         data2 = gather("danish", ["pitch", "voice", "pwr"])
+        
 
     else:
         data1 = gather("skane", ["pitch", "voice", "pwr"])
         data2 = gather("west", ["pitch", "voice", "pwr"])
 
 
-    print(len(X_train), 'train sequences', len(X_val), 'validation sequences', len(X_test), 'test sequences')
+    print(len(X_train), 'train sequences', len(X_test), 'test sequences')
     print('X_train shape:', X_train.shape)
     print('X_test shape:', X_test.shape)
     print('y_train shape:', y_train.shape)
     print('y_test shape:', y_test.shape)
     #print(y_test)
 
-    return X_train[:, :, np.newaxis], X_val[:, :, np.newaxis], X_test[:, :, np.newaxis],y_train, y_val, y_test
+    return X_train,  X_test,y_train, y_test
 
 
-def train_model(X_train, X_val, X_test, y_train,y_val, y_test):
+def train_model(X_train, X_test, y_train, y_test):
     print('Preprocessing...')
     
     print("Mean of training set : ", np.mean(X_train))    
-    print("Mean of validation set : ", np.mean(X_val)) 
+    #print("Mean of validation set : ", np.mean(X_val)) 
     print("Mean of test set : ", np.mean(X_test)) 
     
     print('Build model...')
 
     Y_train = np_utils.to_categorical(np.clip(y_train, 0, 1), nb_classes)
-    Y_val = np_utils.to_categorical(np.clip(y_val, 0, 1), nb_classes)
+    #Y_val = np_utils.to_categorical(np.clip(y_val, 0, 1), nb_classes)
     Y_test = np_utils.to_categorical(np.clip(y_test, 0, 1), nb_classes)
     
 
@@ -102,7 +108,8 @@ def train_model(X_train, X_val, X_test, y_train,y_val, y_test):
     print("Training ...")
     model.fit(X_train, Y_train,
               batch_size=batch_size, epochs=10,
-              validation_data=(X_val, Y_val), 
+              validation_split = 1./6,
+              #validation_data=(X_val, Y_val), 
               #callbacks = [EarlyStopping(patience = 2, verbose = 1)],
               verbose = 1)
     [score, acc] = model.evaluate(X_test, Y_test,
@@ -115,6 +122,6 @@ def train_model(X_train, X_val, X_test, y_train,y_val, y_test):
     #print('Test accuracy: %.2f %%' % (100 - len(y_test[np.nonzero(np.argmax(prediction, axis = 1) - y_test)]) *100 / 50))
     
 if __name__ == "__main__":
-    X_train, X_val, X_test, y_train, y_val, y_test = get_data("test")
-    #X_train, X_val, X_test, y_train, y_val, y_test = get_data("south")
-    #train_model(X_train, X_val, X_test, y_train, y_val, y_test)
+    #X_train, X_val, X_test, y_train, y_val, y_test = get_data("test")
+    X_train, X_test, y_train, y_test = get_data("west_skane")
+    train_model(X_train, X_test, y_train, y_test)
