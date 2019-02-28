0ff7ba1d369392fefbf7fa4f39ce514e6de258b8
Gabriel de Lisle <gldl@kth.se>
2019-02-28 10:30:56 +0100

    update script

--- scripts/download_all_recordings.sh
+++ scripts/download_all_recordings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Downloading recordings in Swedish, Danish and Norwegian"
-sh ./download_from_swedia_skane.sh
-sh ./download_from_swedia_west.sh
+sh ./download_from_lab.sh west
+sh ./download_from_lab.sh skane
 
 sh ./download_from_lab.sh danish
 sh ./download_from_lab.sh norwegian
